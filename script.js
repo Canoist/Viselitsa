99da6a6f2724acae0237d21b947420f440a8900e
Leonid Lichkun <lionproklon@gmail.com>
2021-11-25 21:34:51 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const words = ['Чужестранец', 'Жужжалка', 'Прокрастинация', 'Сватовство', 'Парадигма', 'Код', 'Скороход'];
+
+const gameBoard = document.querySelector('.game__board')
+
+function getRandomWord(words) {
+   
+}
+
